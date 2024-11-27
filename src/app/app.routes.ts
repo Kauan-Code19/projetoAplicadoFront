4b0e62f55338a6ecf1d089fc01e995fb1e7601f1
Kauan-Code19 <pkauprofissional@gmail.com>
2024-11-27 00:23:19 -0300

    adicao da rota windows, criação do componente windows

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WindowsComponent } from './f/windows/windows.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'windows', component: WindowsComponent},
+    { path: '',   redirectTo: '/windows', pathMatch: 'full' }
+];
