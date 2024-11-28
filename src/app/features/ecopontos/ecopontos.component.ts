6457389d2633f69618c196f08b66a71132a64aab
Kauan-Code19 <pkauprofissional@gmail.com>
2024-11-28 00:53:03 -0300

    ajuste no import noramlize, adição da rota ecopontos e criação do componente ecopontos

---
+++ src/app/features/ecopontos/ecopontos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecopontos-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './ecopontos.component.html',
+  styleUrl: './ecopontos.component.css'
+})
+export class EcopontosComponent {
+
+}
