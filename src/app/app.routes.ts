c95180db7dde4a804dd47d481baac334dd21e6f9
Kauan-Code19 <pkauprofissional@gmail.com>
2024-11-28 13:11:43 -0300

    criação da rota dicas e componente dicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { WindowsComponent } from './features/windows/windows.component';
 import { DiasHorariosComponent } from './features/dias-horarios/dias-horarios.component';
 import { EcopontosComponent } from './features/ecopontos/ecopontos.component';
+import { DicasComponent } from './features/dicas/dicas.component';
 
 export const routes: Routes = [
     {path: 'windows', component: WindowsComponent},
     { path: '',   redirectTo: '/windows', pathMatch: 'full' },
     {path: 'dias-horarios', component: DiasHorariosComponent},
-    {path: 'ecopontos', component: EcopontosComponent}
+    {path: 'ecopontos', component: EcopontosComponent},
+    {path: 'dicas', component: DicasComponent}
 ];
---
+++ src/app/features/dicas/dicas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dicas-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './dicas.component.html',
+  styleUrl: './dicas.component.css'
+})
+export class DicasComponent {
+
+}
