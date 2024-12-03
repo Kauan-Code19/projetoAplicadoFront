5bd6f112f2413e8fc2fe2f92c497c0851487f9ba
Kauan-Code19 <pkauprofissional@gmail.com>
2024-12-02 21:15:57 -0300

    criação dos enviroments, integração dos componentes com a api e iteração na view. Criação dos services district e collect. Criação do enum shift e as interfaces de resposta da api.

--- src/app/features/ecopontos/ecopontos.component.ts
+++ src/app/features/ecopontos/ecopontos.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DistrictResponse } from '../../../interfaces/district-response';
+import { DistrictService } from '../../services/district.service';
+import { EcopointResponse } from '../../../interfaces/ecopoint-response';
 
 @Component({
   selector: 'ecopontos-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
+  providers: [DistrictService],
   templateUrl: './ecopontos.component.html',
   styleUrl: './ecopontos.component.css'
 })
-export class EcopontosComponent {
+export class EcopontosComponent implements OnInit {
+  ecopoints: EcopointResponse[] = []
+  districts: DistrictResponse[] = [] 
+  selectedDistrictForViewEcopoint: string = '';
 
+
+  constructor(private districtService: DistrictService) {}
+
+
+  ngOnInit(): void {
+    this.districtService.callDistrictManagementServiceForFetchAllDistricts().subscribe({
+      next: (response) => {
+        this.districts = response;
+      }
+    })
+  }
+
+
+  onDistrictSelected(districtName: string): void {
+    if (districtName) {
+      this.fetchCollectionsByDistrict(districtName);
+    }
+  }
+
+
+  fetchCollectionsByDistrict(district: string) {
+    this.districtService.callDistrictManagementServiceForFetchEcopointsByDistrict(district)
+    .subscribe({
+      next: (response) => {
+        this.ecopoints = response
+      }
+    })
+  }
 }
---
+++ src/interfaces/ecopoint-response.ts
@@ -0,0 +1,5 @@
+export interface EcopointResponse {
+    id: number
+    address: string
+    district: string
+}
